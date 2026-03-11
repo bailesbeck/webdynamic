4cadff9785ea51f1e452c9a1a031a4bb1351faee
bailesbeck <bailesbeck@gmail.com>
2026-03-11 12:21:25 -0600

    hikes fix

--- ponder/hikes.js
+++ ponder/hikes.js
@@ -72,14 +72,129 @@ const hikes = [
   }
 ];
 
-let html = '<div class="hike-card">
+let hikeContainer = document.querySelector('#hike-coontainer');
+let input = document.qeruySelector('#search');
+let button = document.querySelector('button');
+let randomNum = Math.floor(Math.random() * hikes.length);
+button.addEventListener('click', search);
+input.addEventListener('keypress', handleEnter);
+
+function search() {
+  let hikeQuery = input.value;
+
+  let filteredHikes = hikes.filter(function(hike) {
+    return ( 
+        hike.name.toLowerCase().includes(hikeQuery.toLowerCase()) || 
+        hike.description.toLowerCase().includes(hikeQuery.toLowerCase()) ||
+        hike.tags.find(tag => tag.toLowerCase().includes(hikeQuery.toLowerCase()))
+    );
+  }
+
+let html = `<div class="hike-card">
         <div class="hike-content">
-          <h2>${hikes.name}</h2>
+          <h2>${hike.name}</h2>
           <div class="hike-tags">
             <button>Canyon</button>
             <button>Tetons</button>
           </div>
           <p>Beautiful short (or long) hike through Teton Canyon.</p>
           <p><span class="rating" role="img" aria-label="1 out of 5">🥾</span></p>
-        </div>'
+        </div>
+      </div>`;
+                
+console.log();
+
+
+function search() {
+
+    let hikeQuery = document.querySelector('#search').value;
+
+    let filteredHikes = hikes.filter(function(hike){
+        return ( 
+            hike.name.toLowerCase().includes(hikeQuery.toLowerCase()) ||
+            hike.description.toLowerCase().includes(hikeQuery.toLowerCase()) || 
+            hike.tags.find(tag => tag.toLowerCase().includes(hikeQuery.toLowerCase()))
+        );
+    })
+
+    function compareHikes(a,b) {
+    if (a.difficulty < b.difficulty) {
+        return -1;
+    } else if (a.difficulty > b.difficulty) {
+        return 1;
+    }
+    return 0;
+    }
+
+    let sortedHikes = filteredHikes.sort(compareHikes);
+
+    // clear out any previous content
+    hikeContainer.innerHTML = '';
+    // output onto screen
+    sortedHikes.forEach(function(hike){
+      renderHike(hike);
+    })
+}
+
+let hikeContainer = document.querySelector('#hike-container');
+
+let button = document.querySelector('button');
+
+button.addEventListener('click', search);
+
+/* for the enter key to work on search - not just clicking the search button */
+input.addEventListener('keypress', handleEnter);
+function handleEnter(event) {
+  if (event.key === 'Enter') {
+    search();
+  }
+}
+
+let randomNum = Math.floor(Math.random() * hikes.length);
+console.log(randomNum);
+
+function tagTemplate(tags) {
+    return tags.map((tag)=> `<button>${tag}</button>`).join(' ');
+}
+
+function difficultyTemplate(rating) {
+		let html = `<span
+	class="rating"
+	role="img"
+	aria-label="Rating: ${rating} out of 5"
+>  Difficulty: `
+    for (let i = 1; i <= 5; i++) {
+      if (i <= rating) {
+        html += `<span aria-hidden="true" class="icon-boot"> 🥾</span>`
+      } else {
+        html += `<span aria-hidden="true" class="icon-empty">▫️</span>`
+      }			
+    }
+    html += `</span>`
+    return html
+  }
+
+function hikesTemplate(hike) {
+    return `<div class="hike-card">
+  <div class="hike-content">
+    <h2>${hike.name}</h2>
+    <div class="hike-tags">
+      ${tagTemplate(hike.tags)}
+    </div>
+    <p>${hike.description}</p>
+    <p>${difficultyTemplate(hike.difficulty)}</p>
+  </div>
+</div>`
+}
+
+function renderHike(hike) {
+    let html = hikesTemplate(hike);
+    hikeContainer.innerHTML += html
+}
+
+function init() {
+    renderHike(hikes[randomNum]);
+}
+
+init();
                 
