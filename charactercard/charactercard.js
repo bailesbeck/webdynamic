3f234013e7eb744cc8a08836c46944a1f4fc9472
bailesbeck <bailesbeck@gmail.com>
2026-03-22 18:16:34 -0600

    character card

---
+++ charactercard/charactercard.js
@@ -0,0 +1,23 @@
+const character = {
+    name: "Aelric",
+    class: "Warrior",
+    level: 1,
+    health: 100,
+    image: "https://via.placeholder.com/150",
+
+    attacked() {
+        this.health -= 20;
+
+        if (this.health <= 0) {
+            this.health = 0;
+            console.log(this.name + " has died 💀");
+        } else {
+            console.log(this.name + " was attacked! Health is now " + this.health);
+        }
+    },
+
+    levelUp() {
+        this.level += 1;
+        console.log(this.name + " leveled up! Level is now " + this.level);
+    }
+};
